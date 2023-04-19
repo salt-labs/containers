b5b3427716ac4020298b31c3f1ecb60742f44cc7
MAHDTech <MAHDTech@saltlabs.tech>
2023-04-19 21:36:25 +1000

    test: Cachix

--- oci/tanzu/tanzu_1.nix
+++ oci/tanzu/tanzu_1.nix
@@ -25,6 +25,8 @@ in
         Once you have downloaded the file, please use the following command and re-run the installation:
 
         nix-prefetch-url file://\$PWD/${filename}
+
+        If you are using cachix,
       '';
     };
 

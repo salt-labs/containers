575f3a0d2daa1f2f89a69f9994d851bc873ccf12
MAHDTech <MAHDTech@saltlabs.tech>
2023-05-18 09:55:41 +1000

    feat: Bump carvel

--- nix/oci/carvel/carvel.nix
+++ nix/oci/carvel/carvel.nix
@@ -7,43 +7,43 @@
     ytt = pkgs.fetchurl {
       name = "ytt";
       url = "https://github.com/carvel-dev/ytt/releases/download/v0.45.1/ytt-linux-amd64";
-      sha256 = "sha256-LLpey2AtwrzKr9iTB3pm3svfl30XtjOWcnhmjpXUjfs=";
+      sha256 = "sha256-AWBtiFzbzgI0D6rcKag7VBUoC8YQ58/z9jk4XY9QSuo=";
     };
 
     kapp = pkgs.fetchurl {
       name = "kapp";
       url = "https://github.com/carvel-dev/kapp/releases/download/v0.56.0/kapp-linux-amd64";
-      sha256 = "sha256-a1Pg2Gb7PNy3gUdcI5c+q2w3lZ5TwiCUvIH5mIhNdK4=";
+      sha256 = "sha256-J2Sqw4Q1sReKnCMsBytuGEv8iAImhYCrkRoehO68QsE=";
     };
 
     kapp-controller = pkgs.fetchurl {
       name = "kctrl";
       url = "https://github.com/carvel-dev/kapp-controller/releases/download/v0.45.1/kctrl-linux-amd64";
-      sha256 = "sha256-FqXD5/9J1S5hadSlf6v56GLW6vfnuHKTunNquURogDQ=";
+      sha256 = "sha256-hlov/BCjJZfPAmZWOCgp6bM7tC9V+tcDOLTFmqXeM+U=";
     };
 
     kbld = pkgs.fetchurl {
       name = "kbld";
       url = "https://github.com/carvel-dev/kbld/releases/download/v0.37.1/kbld-linux-amd64";
-      sha256 = "sha256-apM/p2qlgbbJLIEMTDWHf61oGH4ukyC4aHbgDsaFIYU=";
+      sha256 = "sha256-8y+xJR/Ltw0ZJUIQUETFKRxuZ/vKf7Rnvw042Sub8gQ=";
     };
 
     imgpkg = pkgs.fetchurl {
       name = "imgpkg";
       url = "https://github.com/carvel-dev/imgpkg/releases/download/v0.36.2/imgpkg-linux-amd64";
-      sha256 = "sha256-LCic9rXIik3UvsF8nlfknCx1McEn6hMHN5RTks3GU2I=";
+      sha256 = "sha256-VpaXZr+yN5esmCXhtXgiFf7gtMbyinuBlrq+TT7Fw2U=";
     };
 
     vendir = pkgs.fetchurl {
       name = "vendir";
       url = "https://github.com/carvel-dev/vendir/releases/download/v0.33.2/vendir-linux-amd64";
-      sha256 = "sha256-DYpF0thWR86TLh1jDUlmjpZVIUCtM8atrV9Ym7gAu4o=";
+      sha256 = "sha256-LMj42zF2hffScCZHErsx77/eFOlbBl8xzR4tjJ7rLTQ=";
     };
   };
 in
   pkgs.stdenv.mkDerivation {
     name = "carvel-dev";
-    version = "1.0.0";
+    version = "1.1.0";
 
     phases = ["installPhase"];
 

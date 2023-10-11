ea611fd75e8f3bba4744bcb6f528bc3012050898
MAHDTech <MAHDTech@saltlabs.tech>
2023-10-11 14:48:12 +1100

    feat: Update Tanzu CLI to v1.0.0 (#60)

--- nix/oci/tanzu/tanzu.nix
+++ nix/oci/tanzu/tanzu.nix
@@ -1,32 +1,23 @@
 # NOTE: Cachix push
 # nix build --json --impure ".#packages.\"x86_64-linux.x86_64-linux\".tanzu" | jq -r '.[0].outputs.out' | cachix push salt-labs
 {pkgs, ...}: let
-  # The Tanzu CLI is now de-coupled from TKG.
-  product_name = "tanzu-cli";
-  product_version = "0.90.1";
-  filename = "tanzu-cli-linux-amd64.tar.gz";
-  checksum = "fec9e268399443de94d1761678aa39be18b7b685dd34a4412933943647b9d0be";
-  homepage = "https://customerconnect.vmware.com/downloads/details?downloadGroup=TCLI-0901&productId=1431";
+  tanzu-cli = {
+    core = pkgs.fetchurl {
+      name = "tanzu-cli";
+      url = "https://github.com/vmware-tanzu/tanzu-cli/releases/download/v1.0.0/tanzu-cli-linux-amd64.tar.gz";
+      sha256 = "sha256-152R0CTIHCc6yhcMnA4hRXp8lvA5xP6t7s5F3M1/F5Q=";
+    };
+
+    plugins = pkgs.fetchurl {
+      name = "tanzu-cli-plugins";
+      url = "https://github.com/vmware-tanzu/tanzu-cli/releases/download/v1.0.0/tanzu-plugins-admin-linux-amd64.tar.gz";
+      sha256 = "sha256-0sHz/f/wR0m67qflHKo0rvVEGI6pjMbRbJH6CcHiGRc=";
+    };
+  };
 in
   pkgs.stdenv.mkDerivation {
-    name = product_name;
-    version = product_version;
-
-    src = pkgs.requireFile {
-      name = filename;
-      sha256 = checksum;
-
-      message = ''
-        In order to use VMware Tanzu CLI, you need to accept the EULA and download the file from:
-
-        ${homepage}
-
-        Once you have downloaded the file, please use the following command and re-run the installation:
-
-        nix-prefetch-url file://\$PWD/${filename}
-        nix-hash --type sha256 --flat --base32 ${filename}
-      '';
-    };
+    name = "tanzu-cli";
+    version = "1.0.0";
 
     dontBuild = true;
     dontConfigure = true;
@@ -38,20 +29,26 @@ in
     unpackPhase = ''
       mkdir --parents unpack
 
-      tar -xzf $src -C unpack
+      tar -xzf ${tanzu-cli.core} -C unpack
+
+      tar -xvf ${tanzu-cli.plugins} -C unpack
     '';
 
     installPhase = ''
       mkdir --parents $out/bin
+      mkdir --parents $out/plugins
 
-      source=unpack/*/tanzu-cli-linux_amd64
+      CORE=unpack/*/tanzu-cli-linux_amd64
+      PLUGINS=unpack/admin-plugins-*
 
-      install --verbose $source $out/bin/tanzu
+      install --mode=0755 --verbose $CORE $out/bin/tanzu
+
+      cp -r $PLUGINS $out/plugins
     '';
 
     meta = {
-      description = "VMware Tanzu CLI";
-      homepage = "https://vmware.com";
-      license = "Commercial";
+      description = "Command line interface for Tanzu Kubernetes Grid.";
+      homepage = "https://github.com/vmware-tanzu/tanzu-cli";
+      license = "Apache 2.0";
     };
   }

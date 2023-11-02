2e9fdb1e7592c19e8d70839f07e876d99385a7f7
MAHDTech <MAHDTech@saltlabs.tech>
2023-11-02 16:00:58 +1100

    fix: chmod 0777 all the things (#67)
    * fix: Chmod 0777 all the things
    
    * fix: Disable Podman

--- flake.nix
+++ flake.nix
@@ -382,13 +382,13 @@
         inherit crossPkgs;
       };
 
-      podman = import ./nix/oci/podman {
-        inherit self;
-        inherit pkgs;
-        inherit pkgsUnstable;
-        inherit crossPkgs;
-        inherit crossPkgsUnstable;
-      };
+      #podman = import ./nix/oci/podman {
+      #  inherit self;
+      #  inherit pkgs;
+      #  inherit pkgsUnstable;
+      #  inherit crossPkgs;
+      #  inherit crossPkgsUnstable;
+      #};
 
       pivnet = import ./nix/oci/pivnet {
         inherit self;

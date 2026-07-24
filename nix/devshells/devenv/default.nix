220e19b2393c5d63b67a35530ec59f9b3dd8c9e7
MAHDTech <MAHDTech@saltlabs.tech>
2026-07-25 00:27:35 +1000

    fix(ci): add task package fallback and remove no-build flag from nix flake check

--- nix/devshells/devenv/default.nix
+++ nix/devshells/devenv/default.nix
@@ -177,6 +177,8 @@ inputs.devenv.lib.mkShell {
         flakesIntegration = true;
       };
 
+      task.package = pkgs.lib.mkDefault (inputs.devenv.packages.${pkgs.stdenv.system}.devenv-tasks or pkgs.hello);
+
       dotenv = {
         enable = true;
         filename = ".env";

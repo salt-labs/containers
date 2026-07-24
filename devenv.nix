7cbabcdd77e058bca0556dade034f432c93829ab
MAHDTech <MAHDTech@saltlabs.tech>
2026-07-25 00:30:45 +1000

    fix(ci): add task package fallback to devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,9 @@
-_: {
+{ pkgs, inputs, ... }: {
   devenv = {
     flakesIntegration = true;
   };
+
+  task.package = pkgs.lib.mkDefault (
+    inputs.devenv.packages.${pkgs.stdenv.system}.devenv-tasks or pkgs.hello
+  );
 }
